9b7a58e1d606c045c7bbff07ff2069308b843234
RautelaZone <anilrautela051090@gmail.com>
2021-02-24 16:06:56 +0530

    [Updated Folders]

--- Programs/HeadFirstPython2010/PageNo21_List_Exercice.py
+++ Programs/HeadFirstPython2010/PageNo21_List_Exercice.py
@@ -3,6 +3,8 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
 "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 for each_item in movies:
-    print(each_item)
-    if isinstance(each_item,list):
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            print(nested_item)
+    else:
         print(each_item)
