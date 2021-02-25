cd55599c429ac70a5acb48f1a1944938d3cdc279
RautelaZone <anilrautela051090@gmail.com>
2021-02-25 23:20:32 +0530

    [Updated Files for HeadFirstPython]

--- Programs/HeadFirstPython2010/nester.py
+++ Programs/HeadFirstPython2010/nester.py
@@ -1,19 +1,17 @@
-'''
-Use of recursion fucntion
-'''
-
-movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
-["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
-
-def print_lol(the_list):
+def print_lol(the_list,indent=False,level=0):
     """ This function takes a positional argument called “the_list", which is any
     Python list (of, possibly, nested lists). Each data item in the provided list
     is (recursively) printed to the screen on its own line
     """
+
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item)        #calling a function from a function-recursive function
+            print_lol(each_item,indent,level+1)  # calling a function from a function-recursive function
         else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
             print(each_item)
-print_lol(movies)
 
+cast = ["Anil","Rautela",["Rautela", "Anil"],"05-Oct-1990"]
+print_lol(cast)
